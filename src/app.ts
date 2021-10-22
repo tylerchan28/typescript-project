3d9165e3712ea2b4626cb0800ab16e0f325e086d
Tyler Chan <tylerchan28@gmail.com>
2021-10-22 11:00:51 -0700

    Add ProjectItem class

--- src/app.ts
+++ src/app.ts
@@ -133,6 +133,27 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// Building based on template
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super("single-project", hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector("h2")!.textContent = this.project.title;
+        this.element.querySelector("h3")!.textContent = this.project.people.toString();
+        this.element.querySelector("p")!.textContent = this.project.description;
+    }
+
+}
 
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
@@ -150,9 +171,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
         listEl.innerHTML = "";
         for (const project of this.assignedProjects) {
-            const listItem = document.createElement("li");
-            listItem.textContent = project.title;
-            listEl.appendChild(listItem)
+            new ProjectItem(this.element.querySelector("ul")!.id, project);
         }
     }
     
